952353f71ab0a1f3bb161fd24f2b1483c0c62a50
Josh Teng <joshteng@webermize.com>
2013-07-07 00:27:36 +0800

    store credit works

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,8 +1,8 @@
 module Spree
   CheckoutController.class_eval do
     before_filter :remove_payments_attributes_if_total_is_zero
-
     private
+
     def remove_payments_attributes_if_total_is_zero
       load_order
 
